251d0fc21784ef2e475a6cdf360993b1dbd15154
GTIGit27 <puga.pascual@free.fr>
2015-12-04 14:15:16 +0100

    session_unset

---
+++ unsetSession.php
@@ -0,0 +1,6 @@
+unset($_SESSION["user"]);
+
+1) créer un mini-site avec 2-3 pages (aide, contact, services)
+
+2) 	authentifié avec : Bonjour Prénom Nom [se déconnecter] -> logoff.php (unset session)
+	non authentifié : [Se déconnecter] [S'inscrire]
